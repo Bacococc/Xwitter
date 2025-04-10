5f6ef2374231861769e69ccf4b2a6bf3387af3ed
Bacococc <pphj7353@naver.com>
2025-04-10 19:43:39 +0900

    update : firebase initialized

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgeDX447ZFhTRn7gdaHWYQp4TlwSmY1WE",
+  authDomain: "xwitter-clone-6b314.firebaseapp.com",
+  projectId: "xwitter-clone-6b314",
+  storageBucket: "xwitter-clone-6b314.firebasestorage.app",
+  messagingSenderId: "818950006986",
+  appId: "1:818950006986:web:5a872e70c2da0ccea52c33"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
